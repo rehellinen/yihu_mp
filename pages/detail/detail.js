8672c52ac99be4198f71e5236fa1bcf7992566e1
rehellinen <912377791@qq.com>
2018-04-07 21:06:15 +0800

    购物车动画效果

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -9,7 +9,8 @@ Page({
     selectedCount: 1,
     cartSelectedCount: 0,
     currentTabsIndex: 0,
-    countArray:[1, 2, 3, 4, 5, 6]
+    countArray:[1, 2, 3, 4, 5, 6],
+    isTap: false
   },
 
   onLoad: function (options) {
@@ -47,18 +48,58 @@ Page({
     })
   },
 
-  addCartTap(){
+  addCartTap(event){
+    // 防止快速点击
+    if(this.data.isTap){
+      return 0;
+    }
+    // 处理加入购物车的数据逻辑
+    this._addCartStorage()
+
+    // 处理加入购物车的动画
+    this._addCartAnimation(event)
+  },
+
+  _addCartAnimation(event){
+    let touches = event.touches[0];
+    let relative = {
+      x: '20px',
+      y: 5 - touches.clientY + 'px'
+    }
+    let style = 'display: block;transform:translate(' + relative.x + ',' + relative.y + ') rotate(350deg) scale(0)';
+    this.setData({
+      isTap: true,
+      translateStyle: style
+    })
+
+    let that = this
+    setTimeout( () => {
+      this.setData({
+        isTap: false,
+        translateStyle: 'transform: none',
+        isShake: true
+      })
+
+      setTimeout( () => {
+        this.setData({
+          cartSelectedCount: cart.getCartTotalCount()
+        })
+        that.setData({
+          isShake: false
+        })
+      }, 200)
+    }, 1000)
+  },
+
+  _addCartStorage(){
     let tempObj = {}
     let keys = ['id', 'name', 'image_id', 'price']
-    for (let key in this.data.product){
-      if(keys.indexOf(key) >= 0){
+    for (let key in this.data.product) {
+      if (keys.indexOf(key) >= 0) {
         tempObj[key] = this.data.product[key]
       }
     }
-    cart.add(tempObj, this.data.selectedCount)
-    this.setData({      
-      cartSelectedCount: cart.getCartTotalCount()
-    })
+    cart.add(tempObj, this.data.selectedCount)    
   },
 
   toCartTap(){
