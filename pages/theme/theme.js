fd78ebad848243c2f4064f23803350334ccc321d
rehellinen <912377791@qq.com>
2018-05-18 18:13:35 +0800

    细节修改

--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,103 +1,101 @@
-import { ThemeModel } from '../../model/ThemeModel.js'
-import { DetailModel } from '../../model/GoodsModel.js'
-let detail = new DetailModel()
+import {ThemeModel} from '../../model/ThemeModel.js'
+import {GoodsModel} from '../../model/GoodsModel.js'
+import {Image} from "../../utils/image"
+
+let goods = new GoodsModel()
 let theme = new ThemeModel()
-let app = getApp()
 
 Page({
-  data: {
-    categoryIndex: 0,
-    loadingHidden: false,
-    className: 'animation1',
-    goods: [],
-    hasMore: [],
-    page: [], 
-    photoCount: 0,
-    loadedPhoto: 0
-  },
+    data: {
+        categoryIndex: 0,
+        loadingHidden: false,
+        className: 'animation1',
+        goods: [],
+        hasMore: [],
+        page: [],
+    },
 
-  onLoad: function (options) {
-    let id = options.id
-    theme.getCategory(id, (res) => {
-      for(let i in res){
-        let id = res[i].id
-        if (res[i].name.length > 3){
-          res[i].fontClass = 'small'
-        }else{
-          res[i].fontClass = 'big'
+    onLoad: function (options) {
+        this.image = new Image(this)
+        this.image.setLoadingHidden()
+
+        let id = options.id
+        theme.getCategory(id, (res) => {
+            for (let i in res) {
+                let id = res[i].id
+                if (res[i].name.length > 3) {
+                    res[i].fontClass = 'small'
+                } else {
+                    res[i].fontClass = 'big'
+                }
+                this.data.goods[id] = []
+                this.data.hasMore[id] = true
+                this.data.page[id] = 1
+            }
+            this.setData({
+                category: res,
+                categoryID: res[0].id,
+            })
+            this._loadGoods()
+        })
+    },
+
+    _loadGoods() {
+        goods.getGoodsByCategoryID(this.data.page[this.data.categoryID], this.data.categoryID, (data) => {
+            for (let i in data) {
+                if (data[i].name.length > 5) {
+                    data[i].name = data[i].name.substr(0, 5)
+                    data[i].name += ' ...'
+                }
+            }
+
+            this.image.addPhotosCount(data.length)
+            this.data.goods[this.data.categoryID].push.apply(this.data.goods[this.data.categoryID], data)
+
+            this.setData({
+                goods: this.data.goods
+            })
+        }, () => {
+            this.data.hasMore[this.data.categoryID] = false
+            this.setData({
+                loadingHidden: true
+            })
+        })
+
+        wx.stopPullDownRefresh()
+    },
+
+    selectCategory(event) {
+        // 切换后的ID和Index
+        let id = event.currentTarget.dataset.id
+        let index = event.currentTarget.dataset.index
+        // 点击当前的选项卡不做操作
+        if (index == this.data.categoryIndex) {
+            return 0
         }
-        this.data.goods[id] = []
-        this.data.hasMore[id] = true
         this.data.page[id] = 1
-      }
-      this.setData({
-        category: res,
-        categoryID: res[0].id,
-      })      
-      this._loadGoods()
-    })    
-    
-  },
-
-  _loadGoods(){
-    detail.getGoodsByCategoryID(this.data.page[this.data.categoryID], this.data.categoryID, (data) => {  
-      for(let i in data){
-        if(data[i].name.length > 5){
-          data[i].name = data[i].name.substr(0, 5)
-          data[i].name += ' ...'
+        this.setData({
+            className: 'animation' + (index + 1),
+            categoryID: id,
+            categoryIndex: index
+        })
+        if (this.data.goods[id].length == 0) {
+            this._loadGoods()
         }
-      }
+    },
 
-      this.data.photoCount += (data.length)    
-      this.data.goods[this.data.categoryID].push.apply(this.data.goods[this.data.categoryID], data)      
-      
-      this.setData({        
-        goods: this.data.goods
-      })
-    }, () => {
-      this.data.hasMore[this.data.categoryID] = false
-      this.setData({
-        loadingHidden: true
-      })
-    })
-  },
+    toLoadMore(event) {
+        if (this.data.hasMore[this.data.categoryID]) {
+            this.data.page[this.data.categoryID]++
+            this._loadGoods()
+        }
+    },
 
-  selectCategory(event){
-    // 切换后的ID和Index
-    let id = event.currentTarget.dataset.id
-    let index = event.currentTarget.dataset.index
-    // 点击当前的选项卡不做操作
-    if (index == this.data.categoryIndex) {
-      return 0
-    }
-    this.data.page[id] = 1    
-    this.setData({
-      className: 'animation' + (index + 1),
-      categoryID: id,
-      categoryIndex: index 
-    })
-    if (this.data.goods[id].length == 0) {
-      this._loadGoods()
-    } 
-  },
+    isLoadedAll(event) {
+        this.image.isLoadedAll()
+    },
 
-  toLoadMore(event){
-    if (this.data.hasMore[this.data.categoryID]){
-      this.data.page[this.data.categoryID]++
-      this._loadGoods()
-    } 
-  },
-
-  isLoadAll(event) {
-    let that = this
-    app.isLoadAll(that)
-  },
-
-  onPullDownRefresh() {
-    this._loadGoods(() => {
-      wx.stopPullDownRefresh()
-    })
-  },
-
-  
+    onPullDownRefresh() {
+        this._loadGoods()
+    },
 })
