1e01df8fcc3dadc46caa8ed492b490246d86090a
rehellinen <912377791@qq.com>
2018-05-18 17:43:38 +0800

    更多订单页面的重构

--- pages/order-more/order-more.js
+++ pages/order-more/order-more.js
@@ -1,87 +1,80 @@
-import { OrderModel } from '../../model/OrderModel.js'
+import {OrderModel} from '../../model/OrderModel.js'
+import {Image} from "../../utils/image"
+
 let order = new OrderModel()
-let app = getApp()
 
 Page({
-  data: {
-    order: [
-      [], [], [], [], []
-    ],
-    hasMore: [true, true, true, true, true],
-    page: [1, 1, 1, 1, 1],
-    loadingHidden: false,
-    photoCount: 0,
-    loadedPhoto: 0,
-    tabIndex: 0,
-  },
+    data: {
+        order: [
+            [], [], [], [], []
+        ],
+        hasMore: [true, true, true, true, true],
+        page: [1, 1, 1, 1, 1],
+        loadingHidden: false,
+        tabIndex: 0,
+    },
 
-  onLoad: function (options) {
-    this._loadOrder()
-    setTimeout(() => {
-      this.setData({
-        loadingHidden: true
-      })
-    }, 5000)
-  },
+    onLoad: function (options) {
+        this.image = new Image(this)
+        this.image.setLoadingHidden()
+        this._loadOrder()
+    },
 
-  onShow() {
-    if (wx.getStorageSync('newOrder')) {
-      this.reload()
-      wx.setStorageSync('newOrder', false)
+    onShow() {
+        if (wx.getStorageSync('newOrder')) {
+            this.reload()
+            wx.setStorageSync('newOrder', false)
+        }
+    },
+
+    onReachBottom() {
+        let index = this.data.tabIndex
+        if (this.data.hasMore[index]) {
+            this.data.page[index]++
+            this._loadOrder()
+        }
+    },
+
+    reload() {
+        this.data.order = [
+            [], [], [], [], []
+        ]
+        this.data.hasMore = [true, true, true, true, true]
+        this.data.page = [1, 1, 1, 1, 1]
+        this._loadOrder()
+    },
+
+    _loadOrder() {
+        let index = this.data.tabIndex
+        order.getOrder(index, this.data.page[index], (res) => {
+            this.image.addPhotosCount(res.length)
+            this.data.order[index].push.apply(this.data.order[index], res)
+            this.setData({
+                order: this.data.order,
+            })
+        }, (res) => {
+            this.data.hasMore[index] = false
+            this.setData({
+                order: this.data.order,
+                loadingHidden: true
+            })
+        })
+        wx.stopPullDownRefresh()
+    },
+
+    isLoadedAll(event) {
+        this.image.isLoadedAll()
+    },
+
+    switchTab(event) {
+        let index = event.detail.index
+        this.data.tabIndex = index
+        if (this.data.order[index].length == 0) {
+            this._loadOrder()
+        }
+    },
+
+    onPullDownRefresh() {
+        this.reload()
     }
-  },
-
-  onReachBottom() {
-    let index = this.data.tabIndex
-    if (this.data.hasMore[index]) {
-      this.data.page[index]++
-      this._loadOrder()
-    }
-  },
-
-  reload(cb) {
-    this.data.order = [
-      [], [], [], [], []
-    ]
-    this.data.hasMore = [true, true, true, true, true]
-    this.data.page = [1, 1, 1, 1, 1]
-    this._loadOrder(cb)
-  },
-
-  _loadOrder(cb) {
-    let index = this.data.tabIndex
-    order.getOrder(index, this.data.page[index], (res) => {
-      this.data.photoCount += res.length
-      this.data.order[index].push.apply(this.data.order[index], res)
-      this.setData({
-        order: this.data.order,
-      })
-    }, (res) => {
-      this.data.hasMore[index] = false
-      this.setData({
-        order: this.data.order,
-        loadingHidden: true
-      })
-    })
-    cb && cb()
-  },
-
-  isLoadAll(event) {
-    let that = this
-    app.isLoadAll(that)
-  },
-
-  switchTab(event) {
-    let index = event.detail.index
-    this.data.tabIndex = index
-    if (this.data.order[index].length == 0) {
-      this._loadOrder()
-    }
-  },
-
-  onPullDownRefresh() {
-    this.reload(() => {
-      wx.stopPullDownRefresh()
-    })
-  }
 })
